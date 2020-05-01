80639aae0d635e2cacd78d76feeeca8ab7ece814
Nikhil <nka@adobe.com>
2020-05-01 16:23:38 +0530

    Created springboot project

---
+++ src/main/java/com/lombok/LombokPoC/LombokPoCApplication.java
@@ -0,0 +1,13 @@
+package com.lombok.LombokPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokPoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lombok/LombokPoC/LombokPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lombok.LombokPoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
